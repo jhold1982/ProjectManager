0a17f255bd63e9e01ceadaaba887eb3277c2fa5f
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-11-27 20:29:18 -0600

    Initial Commit

---
+++ ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by Justin Hold on 11/27/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
