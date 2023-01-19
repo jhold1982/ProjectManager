ff2a7f564aab7a48082e9317d3a17f5e4b75a0ed
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-01-19 13:14:40 -0600

    Added signInWithApple button

--- ProjectManager/Views/ContentView.swift
+++ ProjectManager/Views/ContentView.swift
@@ -12,66 +12,48 @@ struct ContentView: View {
 	@SceneStorage("selectedView") var selectedView: String?
 	@EnvironmentObject var dataController: DataController
 	private let newProjectActivity = "com.lefthandedapps.projectmanager.newProject"
-	@State var progress: CGFloat = 0
-	@State var doneLoading: Bool = false
+	@State private var isLoading = false
 	var body: some View {
 		ZStack {
-			if doneLoading {
-				TabView(selection: $selectedView) {
-					HomeView(dataController: dataController)
-						.tag(HomeView.tag)
-						.tabItem {
-							Label("Home", systemImage: "house")
-						}
-					ProjectsView(dataController: dataController, showClosedProjects: false)
-						.tag(ProjectsView.openTag)
-						.tabItem {
-							Label("Open", systemImage: "list.bullet")
-						}
-					ProjectsView(dataController: dataController, showClosedProjects: true)
-						.tag(ProjectsView.closedTag)
-						.tabItem {
-							Label("Closed", systemImage: "checkmark")
-						}
-					AwardsView()
-						.tag(AwardsView.tag)
-						.tabItem {
-							Label("Awards", systemImage: "rosette")
-						}
-					SharedProjectsView()
-						.tag(SharedProjectsView.tag)
-						.tabItem {
-							Label("Community", systemImage: "person.3")
-						}
-				}
-				.onContinueUserActivity(CSSearchableItemActionType, perform: moveToHome)
-				.onContinueUserActivity(newProjectActivity, perform: createProject)
-				.userActivity(newProjectActivity) { activity in
-					activity.title = "New Project"
-					activity.isEligibleForPrediction = true
-				}
-				.onOpenURL(perform: openURL)
-			} else {
-				LoadingView(content: Image("LaunchIcon")
-								.resizable()
-								.scaledToFit()
-								.padding(.horizontal),
-							progress: $progress)
-				// Added to simulate asynchronous data loading
-				.onAppear {
-					DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-						withAnimation {
-							self.progress = 0
-						}
-						DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
-							withAnimation {
-								self.doneLoading = true
-							}
-						}
+			TabView(selection: $selectedView) {
+				HomeView(dataController: dataController)
+					.tag(HomeView.tag)
+					.tabItem {
+						Label("Home", systemImage: "house")
 					}
+				ProjectsView(dataController: dataController, showClosedProjects: false)
+					.tag(ProjectsView.openTag)
+					.tabItem {
+						Label("Open", systemImage: "list.bullet")
+					}
+				ProjectsView(dataController: dataController, showClosedProjects: true)
+					.tag(ProjectsView.closedTag)
+					.tabItem {
+						Label("Closed", systemImage: "checkmark")
+					}
+				AwardsView()
+					.tag(AwardsView.tag)
+					.tabItem {
+						Label("Awards", systemImage: "rosette")
+					}
+				SharedProjectsView()
+					.tag(SharedProjectsView.tag)
+					.tabItem {
+						Label("Community", systemImage: "person.3")
+					}
+				if isLoading {
+					LoadingView()
 				}
 			}
+			.onContinueUserActivity(CSSearchableItemActionType, perform: moveToHome)
+			.onContinueUserActivity(newProjectActivity, perform: createProject)
+			.userActivity(newProjectActivity) { activity in
+				activity.title = "New Project"
+				activity.isEligibleForPrediction = true
+			}
+			.onOpenURL(perform: openURL)
 		}
+		.onAppear { startNetworkCall() }
 	}
 	func moveToHome(_ input: Any) {
 		selectedView = HomeView.tag
@@ -84,7 +66,14 @@ struct ContentView: View {
 		selectedView = ProjectsView.openTag
 		dataController.addProject()
 	}
+	func startNetworkCall() {
+		isLoading = true
+		DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+			isLoading = false
+		}
+	}
 }
+
 struct ContentView_Previews: PreviewProvider {
 	static var dataController = DataController.preview
 	static var previews: some View {
@@ -93,40 +82,15 @@ struct ContentView_Previews: PreviewProvider {
 			.environmentObject(dataController)
 	}
 }
-struct ScaledMaskModifier<Mask: View>: ViewModifier {
-	var mask: Mask
-	var progress: CGFloat
-	func body(content: Content) -> some View {
-		content
-			.mask(GeometryReader(content: { geometry in
-				self.mask.frame(width: self.calculateSize(geometry: geometry) * self.progress,
-								height: self.calculateSize(geometry: geometry) * self.progress,
-								alignment: .center)
-			}))
-	}
-	// Calculate max size of Mask
-	func calculateSize(geometry: GeometryProxy) -> CGFloat {
-		if geometry.size.width > geometry.size.height {
-			return geometry.size.width
-		}
-		return geometry.size.height
-	}
-}
-struct LoadingView<Content: View>: View {
-	var content: Content
-	@Binding var progress: CGFloat
-	@State var logoOffset: CGFloat = 0
+
+struct LoadingView: View {
 	var body: some View {
-		content
-			.modifier(ScaledMaskModifier(mask: Circle(), progress: progress))
-			.offset(x: 0, y: logoOffset)
-			.onAppear {
-				withAnimation(Animation.easeInOut(duration: 1)) {
-					self.progress = 1.0
-				}
-				withAnimation(Animation.easeInOut(duration: 0.4).repeatForever(autoreverses: true)) {
-					self.logoOffset = 10
-				}
-			}
+		ZStack {
+			Color(.systemBackground)
+				.ignoresSafeArea()
+			ProgressView()
+				.progressViewStyle(CircularProgressViewStyle(tint: .red))
+				.scaleEffect(10)
+		}
 	}
 }
---
+++ ProjectManager/Views/SignInView.swift
@@ -0,0 +1,74 @@
+//
+//  SignInView.swift
+//  ProjectManager
+//
+//  Created by Justin Hold on 1/19/23.
+//
+
+import AuthenticationServices
+import SwiftUI
+
+struct SignInView: View {
+	enum SignInStatus {
+		case unknown
+		case authorized
+		case failure(Error?)
+	}
+	@State private var status = SignInStatus.unknown
+    var body: some View {
+		NavigationStack {
+			Group {
+				switch status {
+				case .unknown:
+					VStack(alignment: .leading) {
+						ScrollView {
+							Spacer()
+							Spacer()
+							Text("""
+	In order to keep our community safe,
+	we ask that you sign in before
+	commenting on a project.
+
+	We don't track your personal information;
+	your name is used for display
+	purposes only.
+		  
+	Please note: we reserve the right to
+	remove messages that are
+	inappropriate or offensive.
+""")
+						}
+						Spacer()
+						SignInWithAppleButton(onRequest: configureSignIn, onCompletion: completeSignIn)
+							.frame(height: 44)
+							.clipShape(Capsule())
+							.padding()
+						Button("Cancel", action: {})
+							.frame(maxWidth: .infinity)
+							.padding()
+					}
+				case .authorized:
+					Text("You're all set!")
+				case .failure(let error):
+					if let error = error {
+						Text("Sorry, there was an error: \(error.localizedDescription)")
+					} else {
+						Text("Sorry, there was an error.")
+					}
+				}
+			}
+		}
+    }
+	func configureSignIn(_ request: ASAuthorizationAppleIDRequest) {
+		
+	}
+	func completeSignIn(_ result: Result<ASAuthorization, Error>) {
+		
+	}
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
